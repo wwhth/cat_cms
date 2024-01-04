831b8fa5c50670f1a22fd7fddf797a17eff2ef33
zhangyunzhi-web <318866560@qq.com>
2024-01-05 00:23:22 +0800

    chore:token设置

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,36 @@
 const KoaRouter = require("@koa/router");
+const jwt = require("jsonwebtoken");
 const userController = require("../controller/user");
 const { verifyUser, handlePassword } = require("../middleware/user.middleware");
 
 const userRouter = new KoaRouter({
   prefix: "/api/v1/user"
 });
+const secret = 'wuwuyueyuezhangyunzhizhangqingyiyangchundi'
 userRouter.post("/register", verifyUser, handlePassword, userController.register);
-
+userRouter.get("/setToken", (ctx) => {
+  // 设置token
+  const payload = {
+    name: "lucy",
+    id: 123,
+  }
+  const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+  ctx.body = {
+    code: 200,
+    message: "ok",
+    data: token
+  }
+});
+userRouter.get("/getToken", (ctx) => {
+  // 获取token
+  console.log(ctx.headers.authorization)
+  const token = ctx.headers.authorization.replace("Bearer ", "");
+  const payload = jwt.verify(token, secret);
+  ctx.body = {
+    code: 200,
+    message: "ok",
+    data: payload
+  }
+}
+)
 module.exports = userRouter;
