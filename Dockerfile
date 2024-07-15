126ecf5a77b3bb6672809e03cc1e6bc3d140e7df
zhangyunzhi230539 <zhangyunzhi@vrvmail.com.cn>
2024-07-15 09:41:53 +0800

    add: 配置pm2

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,10 @@ WORKDIR /home/blog_backed
 
 #  将Dockerfile当前目录下所有文件拷贝至容器内项目目录并安装项目依赖
 COPY . /home/blog_backed
+RUN npm install pm2 -g
 RUN npm install
-
 # 容器对外暴露的端口号，要和node项目配置的端口号一致
 EXPOSE 3001
 
 # 容器启动时执行的命令
-CMD [ "node", "src/main.js" ]
+CMD [ "npm", "start" ]
--- src/app/index.js
+++ src/app/index.js
@@ -8,14 +8,15 @@ const cors = require("koa2-cors");
 const app = new Koa();
 app.use(
   cors({
-    origin: function (ctx) {
-      //设置允许来自指定域名请求
-      if (ctx.url === "/test") {
-        return "*"; // 允许来自所有域名请求
-      }
-      return "*"; //只允许http://localhost:5173这个域名的请求
-      // return "http://localhost:5173"; //只允许http://localhost:5173这个域名的请求
-    },
+    origin: "*",
+    // origin: function (ctx) {
+    //   //设置允许来自指定域名请求
+    //   if (ctx.url === "/test") {
+    //     return "*"; // 允许来自所有域名请求
+    //   }
+    //   return "*"; //只允许http://localhost:5173这个域名的请求
+    //   // return "http://localhost:5173"; //只允许http://localhost:5173这个域名的请求
+    // },
     maxAge: 5, //指定本次预检请求的有效期，单位为秒。
     credentials: true, //是否允许发送Cookie
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], //设置所允许的HTTP请求方法
