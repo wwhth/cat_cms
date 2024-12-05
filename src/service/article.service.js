e27bf94357d1650259971e989f8f97d70dc51432
wwhth <zhangyunzhi@vrvmail.com.cn>
2024-12-05 17:10:23 +0800

    feature:添加文章类型

--- src/service/article.service.js
+++ src/service/article.service.js
@@ -5,10 +5,11 @@ class ArticleService {
   async getAll() {
     const articleList = await connection
       .query(
-        "select ba.id as id, ba.title as title,ba.intro as introduce, ba.author as userid,ua.`name` as username,ba.created_at as date, bc.id as category_id,bc.category_name as category,bl.label_name as label,bl.id as label_id,ba.content as content from blog_article ba LEFT JOIN blog_category bc ON  ba.category_id = bc.id LEFT JOIN blog_label bl on ba.label_id =bl.id LEFT JOIN user_account ua on ba.author = ua.id;"
+        "select ba.id as id, ba.title as title,ba.intro as introduce, ba.artical_type as artical_type, ba.author as userid,ua.`name` as username,ba.created_at as date, bc.id as category_id,bc.category_name as category,bl.label_name as label,bl.id as label_id,ba.content as content from blog_article ba LEFT JOIN blog_category bc ON  ba.category_id = bc.id LEFT JOIN blog_label bl on ba.label_id =bl.id LEFT JOIN user_account ua on ba.author = ua.id ORDER BY ba.created_at DESC;"
       )
       .then((res) => {
         const [values] = res;
+        console.log("🚀 ~ ArticleService ~ .then ~ values:", values)
         return values;
       })
       .catch((err) => {
@@ -18,7 +19,7 @@ class ArticleService {
   }
   async getArticleById(id) {
     const article = await connection
-      .query("select ba.id as id, ba.title as title,ba.intro as introduce, ba.author as userid,ua.`name` as username,ba.created_at as date, bc.id as category_id,bc.category_name as category,bl.label_name as label,bl.id as label_id,ba.content as content from blog_article ba left join blog_label bl on ba.label_id = bl.id left join blog_category bc on ba.category_id = bc.id LEFT JOIN user_account ua on ba.author = ua.id where ba.id = ?;", [id])
+      .query("select ba.id as id, ba.title as title,ba.intro as introduce,ba.artical_type as artical_type , ba.author as userid,ua.`name` as username,ba.created_at as date, bc.id as category_id,bc.category_name as category,bl.label_name as label,bl.id as label_id,ba.content as content from blog_article ba left join blog_label bl on ba.label_id = bl.id left join blog_category bc on ba.category_id = bc.id LEFT JOIN user_account ua on ba.author = ua.id where ba.id = ?;", [id])
       .then((res) => {
         console.log("🚀 ~ ArticleService ~ article ~ res:", res);
         const values = res[0];
@@ -30,8 +31,8 @@ class ArticleService {
     return article;
   }
   async createArticle(body) {
-    const { title, userid, content, category_id, label_id } = body;
-    console.log("%c Line:34 🍪 title, userid, content, category_id, label_id", "color:#ea7e5c", title, userid, content, category_id, label_id);
+    const { title, userid, content, category_id, label_id,artical_type } = body;
+    console.log("%c Line:34 🍪 title, userid, content, category_id, label_id", "color:#ea7e5c", title, userid, content, category_id, label_id,artical_type);
     const result = await connection
       .query("insert into blog_article (title,author,content,category_id,label_id) values (?,?,?,?,?)", [
         title,
@@ -74,9 +75,10 @@ class ArticleService {
     return result;
   }
   async updateArticle(body) {
-    const { id, title, content, category_id, label_id, intro } = body;
+    const { id, title, content, category_id, label_id, intro,artical_type } = body;
+    console.log("🚀 ~ ArticleService ~ updateArticle ~ artical_type:", artical_type)
     const result = await connection
-      .query("update blog_article set title = ?,intro =?,content = ?,category_id = ?,label_id = ? where id = ?", [title, intro, content, category_id, label_id, id])
+      .query("update blog_article set title = ?,intro =?,content = ?,category_id = ?,label_id = ?,artical_type =?  where id = ?", [title, intro, content, category_id, label_id,artical_type, id])
       .then((_) => {
         return {
           code: 200,
@@ -94,7 +96,7 @@ class ArticleService {
   async searchArticle() { }
   async getArticleByLabelId(id) {
     const article = await connection
-      .query("select ba.id as id, ba.title as title,ba.intro as introduce, ba.author as userid,ua.`name` as username,ba.created_at as date, bc.id as category_id,bc.category_name as category,bl.label_name as label,bl.id as label_id,ba.content as content from blog_article ba left join blog_label bl on ba.label_id = bl.id left join blog_category bc on ba.category_id = bc.id LEFT JOIN user_account ua on ba.author = ua.id where bl.id = ?;", [id])
+      .query("select ba.id as id, ba.title as title,ba.intro as introduce, ba.artical_type as artical_type, ba.author as userid,ua.`name` as username,ba.created_at as date, bc.id as category_id,bc.category_name as category,bl.label_name as label,bl.id as label_id,ba.content as content from blog_article ba left join blog_label bl on ba.label_id = bl.id left join blog_category bc on ba.category_id = bc.id LEFT JOIN user_account ua on ba.author = ua.id where bl.id = ?;", [id])
       .then((res) => {
         const values = res[0];
         return values;
